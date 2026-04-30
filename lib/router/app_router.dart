e55e694447945de886688d930c436161ae04af13
date300 <date300us@gmail.com>
2026-04-30 02:24:00 +0000

    Update: description of changes

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,4 +1,4 @@
-,import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../features/vendor_apply/vendor_apply_page.dart';
